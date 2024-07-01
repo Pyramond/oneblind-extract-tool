d10f3ba735904cd2b0efadb5ee636092c6c06eab
Pyramond <dayamond@protonmail.com>
2024-07-01 15:40:53 +0200

    Remove chalk lib

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-import Docker from "dockerode"
-import fs from "fs"
-import path from "path"
-import tar from "tar-stream"
-import readline from "readline"
-import chalk from "chalk"
+// import Docker from "dockerode"
+// import fs from "fs"
+// import path from "path"
+// import tar from "tar-stream"
+// import readline from "readline"
+// import chalk from "chalk"
+const Docker = require("dockerode")
+const fs = require("fs")
+const path = require("path")
+const tar = require("tar-stream")
+const readline = require("readline")
 
 
 const DATABASE_PATH = "/app/src/static/database.db"
 const AVATARS_PATH = "/app/src/static/avatars/custom"
 const ENV_PATH = "/app/.env"
 
+const RESET = '\x1b[0m';
+const GREEN = '\x1b[32m';
+const CYAN = '\x1b[36m';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -38,7 +46,9 @@ async function copyFiles(containerFilePath, container, extractPath, name) {
         })
 
         extract.on("finish", () => {
-            console.log(chalk.green(`${container.id}`) + ":" + chalk.cyan(`${name}`) + ` => extraction terminée`)
+            // console.log(chalk.green(`${container.id}`) + ":" + chalk.cyan(`${name}`) + ` => extraction terminée`)
+            console.log(`${GREEN}${container.id}${RESET}:${CYAN}${name}${RESET} => extraction terminée`)
+            // console.log("ter")
         })
 
         stream.pipe(extract)
